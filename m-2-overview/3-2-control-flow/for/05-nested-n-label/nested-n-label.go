87ec5279c5f8d90d791d122204eeabc1a865df9b
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-09-12 22:54:33 +0400

    add nested-n-label.go

---
+++ m-2-overview/3-2-control-flow/for/05-nested-n-label/nested-n-label.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 'C'-style 'for'
+
+outerLoopLabel:
+	for i := 0; i < 4; i++ {
+		fmt.Printf("i = %d\n", i)
+		fmt.Println("  |")
+		for j := 0; j < 4; j++ {
+			if i*j > 4 {
+				//break // only breaks internal loop 'for', and then continues outer 'for'
+				break outerLoopLabel // breaks outer 'for' from internal 'for'. Last line: |=> j = 2, i*j = 4
+			}
+			fmt.Printf("  |=> j = %d, i*j = %d\n", j, i*j)
+		}
+	}
+}
