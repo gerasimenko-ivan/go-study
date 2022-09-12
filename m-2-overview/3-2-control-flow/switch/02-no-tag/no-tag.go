2dc98655d25bfa70acb420b1f3c645a3854711ed
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-09-12 22:21:43 +0400

    add comment

--- m-2-overview/3-2-control-flow/switch/02-no-tag/no-tag.go
+++ m-2-overview/3-2-control-flow/switch/02-no-tag/no-tag.go
@@ -9,7 +9,7 @@ func main() {
 	input := inp.ReadLine("Enter 'yes' or 'no' (or 'y'/'n'): ")
 
 	switch { // No tag
-	case input == "yes" || input == "y": // condition
+	case input == "yes" || input == "y": // condition, could be complex (not like 'tag == case-value')
 		fmt.Println("YES entered")
 	case input == "no" || input == "n":
 		fmt.Println("NO entered")
