c0706b697ec59538759869eba99937223dddf1cd
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-09-12 20:53:16 +0400

    add condition-only.go, move init-condition-update.go

---
+++ m-2-overview/3-2-control-flow/for/02-condition-only/condition-only.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/* Condition only 'for' looks like a BAD STYLE.
+	Reasons:
+	 - you can forget '++' part & get infinite loop
+	 - you have to search for '++' part in code (if 'for' is more then one line, this takes time)
+	*/
+
+	i := 0
+	for i < 4 {
+		fmt.Printf("i = %d\n", i)
+		i++
+	}
+	fmt.Printf("Print i = %d one more time (outside the loop)\n", i)
+}
