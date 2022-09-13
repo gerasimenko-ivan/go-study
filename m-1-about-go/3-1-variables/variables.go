6a7902ee5eace63b8dbbdef9df5b00f15f355c89
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-09-13 13:41:13 +0400

    add variables.go

---
+++ m-1-about-go/3-1-variables/variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var - keyword
+	// x - variable name
+	// int - variable type
+	// = 10 - variable value
+	var x int = 10
+
+	// NO TYPE
+	var y = 11
+
+	fmt.Printf("x = %d, y = %d", x, y)
+}
