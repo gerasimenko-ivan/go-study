0184b6ea75ab46cd4bd5ee77b6346fc4a9792117
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-09-13 12:46:13 +0400

    change break condition for clear understanding

--- m-2-overview/3-2-control-flow/for/05-nested-n-label/nested-n-label.go
+++ m-2-overview/3-2-control-flow/for/05-nested-n-label/nested-n-label.go
@@ -10,11 +10,11 @@ outerLoopLabel:
 		fmt.Printf("i = %d\n", i)
 		fmt.Println("  |")
 		for j := 0; j < 4; j++ {
-			if i*j > 4 {
+			if i+j > 3 {
 				//break // only breaks internal loop 'for', and then continues outer 'for'
 				break outerLoopLabel // breaks outer 'for' from internal 'for'. Last line: |=> j = 2, i*j = 4
 			}
-			fmt.Printf("  |=> j = %d, i*j = %d\n", j, i*j)
+			fmt.Printf("  |=> j = %d, i+j = %d\n", j, i+j)
 		}
 	}
 }
