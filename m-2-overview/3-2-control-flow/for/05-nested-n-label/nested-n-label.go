c8e9ddf439f8912f0f6d93241c536f8f6f97d010
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-09-13 12:51:45 +0400

    add loops version with break without label

--- m-2-overview/3-2-control-flow/for/05-nested-n-label/nested-n-label.go
+++ m-2-overview/3-2-control-flow/for/05-nested-n-label/nested-n-label.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	// 'C'-style 'for'
 
 outerLoopLabel:
 	for i := 0; i < 4; i++ {
@@ -11,8 +10,24 @@ outerLoopLabel:
 		fmt.Println("  |")
 		for j := 0; j < 4; j++ {
 			if i+j > 3 {
+				fmt.Println("  BREAK to LABEL")
 				//break // only breaks internal loop 'for', and then continues outer 'for'
 				break outerLoopLabel // breaks outer 'for' from internal 'for'. Last line: |=> j = 2, i*j = 4
+			}
+			fmt.Printf("  |=> j = %d, i+j = %d\n", j, i+j)
+		}
+	}
+
+	fmt.Println("")
+	fmt.Println("  +++++++++++ ")
+	fmt.Println("  BREAK without LABEL")
+	for i := 0; i < 4; i++ {
+		fmt.Printf("i = %d\n", i)
+		fmt.Println("  |")
+		for j := 0; j < 4; j++ {
+			if i+j > 3 {
+				fmt.Println("  BREAK (only internal loop)")
+				break // only breaks internal loop 'for', and then continues outer 'for'
 			}
 			fmt.Printf("  |=> j = %d, i+j = %d\n", j, i+j)
 		}
