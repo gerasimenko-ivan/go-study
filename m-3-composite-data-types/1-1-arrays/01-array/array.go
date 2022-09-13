d47647ec8755ea10b35dfa7e3444aa5e8cd41de5
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-09-13 18:26:42 +0400

    add array declaration examples

--- m-3-composite-data-types/1-1-arrays/01-array/array.go
+++ m-3-composite-data-types/1-1-arrays/01-array/array.go
@@ -3,6 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	// fixed-length / same data-type
+
+	// full declaration (redundant)
 	var point [3]float64 = [3]float64{1, 2, 3}
 	fmt.Println(point)
+
+	// split declaration & initialization
+	var x [5]int
+	x[0] = 9
+	fmt.Println(x)
+
+	// no explicit length
+	y := [...]int{3, 33, 333}
+	fmt.Println(y)
 }
