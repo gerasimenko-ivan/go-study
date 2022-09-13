d6d0d0514dc3c6fe8bf5414cf58575032f9b120f
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-09-13 14:20:20 +0400

    add int-types.go

---
+++ m-1-about-go/3-1-variables/04-int-types/int-types.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var integer8 int8 = -128         // -128; 127
+	var unsignedInteger8 uint8 = 255 // 0; 255
+	// (u)int 16, 32, 64
+	// byte = uint8
+	// rune
+	var b byte = 255        // 0; 255
+	var r rune = 2147483647 // -2147483648; 2147483647
+
+	fmt.Printf("integer8 = %d\n", integer8)
+	fmt.Printf("unsignedInteger8 = %d\n", unsignedInteger8)
+	fmt.Printf("b = %d\n", b)
+	fmt.Printf("r = %d\n", r)
+}
