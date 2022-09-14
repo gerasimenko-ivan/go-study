0711279dd47efbe4e3df1a968f817de918752035
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-09-14 22:41:58 +0400

    add slice.go

---
+++ m-3-composite-data-types/1-2-slice/01-slice/slice.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// length - real number of elements in s slice
+	// capacity - number of elements allocated in memory for slice
+	//   capacity extends by x2 (present value x2) when len > cap after adding slice elements
+	s := []int{1, 2, 3}
+	fmt.Printf("s len = %d, s cap = %d\n", len(s), cap(s)) // s len = 3, s cap = 3
+	s = append(s, 4)
+	fmt.Printf("s len = %d, s cap = %d\n", len(s), cap(s)) // s len = 4, s cap = 6
+	s = append(s, 5, 6, 7)
+	fmt.Printf("s len = %d, s cap = %d\n", len(s), cap(s)) // s len = 7, s cap = 12
+}
