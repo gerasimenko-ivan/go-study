506b91e1875eb8958fb3ae1176710314a79e5ee4
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-09-15 23:02:27 +0400

    from-array.go shows only creation slice from array now

--- m-3-composite-data-types/1-2-slice/02-slice-from-array/from-array.go
+++ m-3-composite-data-types/1-2-slice/02-slice-from-array/from-array.go
@@ -10,16 +10,11 @@ func main() {
 	//  - 'to' - to element 3 (excluding)     - so length   - 2
 	//  - length - number of elements "copied" to slice "from"-"to"
 	//  - capacity - length of array from "from" to the end of the array
-	fmt.Printf("slice len = %d, s cap = %d\n", len(slice), cap(slice)) // slice len = 2, s cap = 4
+
+	// slice len = 2, s cap = 4
+	fmt.Printf("slice len = %d, s cap = %d\n", len(slice), cap(slice))
 
 	slice[0] = 222
-	fmt.Println(array) // array is still underlying slice, so output: [1 222 3 4 5]
 	fmt.Println(slice) // output: [222 3]
-
-	sla_2 := array[2:4]
-	fmt.Println(sla_2) // output: [3 4]
-	sla_2[0] = 333
-	fmt.Println(array) // array is still underlying slices, so output: [1 222 333 4 5]
-	fmt.Println(slice) // output: [222 333]
-	fmt.Println(sla_2) // output: [333 4]
+	fmt.Println(array) // array is still underlying slice, so output: [1 222 3 4 5]
 }
