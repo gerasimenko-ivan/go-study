0514e3cd1cbd12447d549d08f02868fb74d5e173
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-09-15 23:32:19 +0400

    demo of removing an element of slice

--- m-3-composite-data-types/1-2-slice/02-slice-from-array/01-int-array/int-array.go
+++ m-3-composite-data-types/1-2-slice/02-slice-from-array/01-int-array/int-array.go
@@ -10,12 +10,17 @@ func main() {
 	fmt.Println(sla2) // [3 4]
 
 	sla1[1] = 333
-	fmt.Println(arr)  // [1 2 333 4 5]
-	fmt.Println(sla1) // [2 333]
-	fmt.Println(sla2) // [333 4]
+	fmt.Println(arr)  // [1 2 333 4 5] - AFFECTED!
+	fmt.Println(sla1) // [2 333]       - we change it
+	fmt.Println(sla2) // [333 4]       - AFFECTED!
 
 	sla1 = append(sla1, 444)
 	fmt.Println(arr)  // [1 2 333 444 5] - AFFECTED!
-	fmt.Println(sla1) // [2 333 444]
+	fmt.Println(sla1) // [2 333 444]     - we change it
 	fmt.Println(sla2) // [333 444]       - AFFECTED!
+
+	sla1 = append(sla1[:0], sla1[1:]...) // remove 0 element
+	fmt.Println(arr)                     // [1 333 444 444 5] - AFFECTED!
+	fmt.Println(sla1)                    // [333 444]         - we change it
+	fmt.Println(sla2)                    // [444 444]         - AFFECTED!
 }
