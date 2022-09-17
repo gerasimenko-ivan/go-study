677672c382b59f0309953716696003c46bb16d35
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-09-17 20:09:33 +0400

    add good style of empty slice creation

--- m-3-composite-data-types/1-2-slice/06-slice-no-elements/slice-no-elements.go
+++ m-3-composite-data-types/1-2-slice/06-slice-no-elements/slice-no-elements.go
@@ -3,8 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	// WARNING! Bad style!
+	// We should expect average number of element that will be added later
 	var sla []string // equivalent to:
 	//sla := []string{}
 	fmt.Println(sla)
-	fmt.Printf("len = %d, cap = %d", len(sla), cap(sla))
+	fmt.Printf("slice %v, len = %d, cap = %d\n", sla, len(sla), cap(sla))
+
+	// GOOD Style
+	// We expect to add around 10 elements later (so there will be NO memory reallocation)
+	good_sla := make([]string, 0, 10)
+	fmt.Printf("slice %v, len = %d, cap = %d", good_sla, len(good_sla), cap(good_sla))
 }
