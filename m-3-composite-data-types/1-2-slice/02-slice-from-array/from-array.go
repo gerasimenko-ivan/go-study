6a9fe5e9c196ed81d81dd1ea501c9d5322eb1897
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-09-17 19:54:41 +0400

    add demonstration of capacity changing

--- m-3-composite-data-types/1-2-slice/02-slice-from-array/from-array.go
+++ m-3-composite-data-types/1-2-slice/02-slice-from-array/from-array.go
@@ -4,15 +4,17 @@ import "fmt"
 
 func main() {
 	array := [...]int{1, 2, 3, 4, 5}
-	slice := array[1:3] // array[from:to]
-	// creates slice from array
-	//  - 'from' - from element 1 (including) - so capacity - 4
-	//  - 'to' - to element 3 (excluding)     - so length   - 2
+	// array[i:j] - creates slice from array
+	//  - 'from' - from element i (including) - so capacity - 4
+	//  - 'to' - to element j (excluding)     - so length   - 2
 	//  - length - number of elements "copied" to slice "from"-"to"
 	//  - capacity - length of array from "from" to the end of the array
-
+	slice := array[1:3] // array[from:to]
 	// slice len = 2, s cap = 4
-	fmt.Printf("slice len = %d, s cap = %d\n", len(slice), cap(slice))
+	fmt.Printf("slice %v len = %d, s cap = %d\n", slice, len(slice), cap(slice))
+
+	slice1 := array[1:3:3] // array[from:to:capacity_to]
+	fmt.Printf("slice %v len = %d, s cap = %d (Capacity is DIFFERENT!)\n", slice1, len(slice1), cap(slice1))
 
 	sla1 := array[:]  // make slice from FULL array
 	fmt.Println(sla1) // [1 2 3 4 5]
