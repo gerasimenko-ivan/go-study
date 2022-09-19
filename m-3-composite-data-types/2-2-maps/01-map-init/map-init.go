f8b983b953d54811fd9b7efd8d6575cd82640bc8
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-09-19 22:02:10 +0300

    add map-init.go

---
+++ m-3-composite-data-types/2-2-maps/01-map-init/map-init.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// init using make
+	idMap := make(map[string]int64)
+	idMap["Tim"] = 123
+	fmt.Println(idMap)
+
+	// init using map literal
+	idMap2 := map[string]int64{}
+	idMap2["Tom"] = 234
+	fmt.Println(idMap2)
+	// or with values
+	idMap3 := map[string]int64{"Tor": 345}
+	fmt.Println(idMap3)
+}
