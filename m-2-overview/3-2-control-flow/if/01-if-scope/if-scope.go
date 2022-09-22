dc58c8d27ecfe3281f2c01a0686688d00ec0f1c6
ivgerasimenko <ivgerasimenko@ozon.ru>
2022-09-22 22:28:13 +0300

    add more demo of scope

--- m-2-overview/3-2-control-flow/if/01-if-scope/if-scope.go
+++ m-2-overview/3-2-control-flow/if/01-if-scope/if-scope.go
@@ -12,10 +12,12 @@ func main() {
 	if today.Before(tomorrow) {
 		fmt.Printf("\nToday '%v' is before tomorrow '%v'\n", today.Format(mmddyyyyFmt), tomorrow.Format(mmddyyyyFmt))
 	}
-	fmt.Printf("Variable 'tomorrow' is seen here.\n\n")
+	fmt.Printf("Variable 'tomorrow = %v' is seen here.\n\n", tomorrow.Format(mmddyyyyFmt))
 
 	if yesterday := today.Add(-24 * time.Hour); yesterday.Before(today) {
 		fmt.Printf("yesterday '%v' is before today '%v'\n", yesterday.Format(mmddyyyyFmt), today.Format(mmddyyyyFmt))
+	} else {
+		fmt.Printf("Variable 'yesterday' is also seen here '%v'.\n", tomorrow.Format(mmddyyyyFmt))
 	}
 	fmt.Printf("Variable 'yesterday' is NOT seen here (ONLY INSIDE IF).\n")
 }
